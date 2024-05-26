98322cfba0dbdca7a2744637e60924d0552f5184
Wouter Dijks <wouterdijks@hotmail.com>
2024-05-26 15:29:21 +0200

    test impl I18n

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module Bookify
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Amsterdam"
+    config.i18n.available_locales = %i[en nl]
+    config.i18n.default_locale = :en
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
