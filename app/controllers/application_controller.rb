1f16bbdf73d3a7588629173d94adc367aa081345
Wouter Dijks <wouterdijks@hotmail.com>
2024-05-26 20:24:06 +0200

    switching locales now possible

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def switch_locale
+    locale = params[:locale]
+    if I18n.available_locales.map(&:to_s).include?(locale)
+      session[:locale] = locale
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
